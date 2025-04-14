2f9fd37eed3b9225db722876a681da87e5ef29ce
JEscGon <JesusE.G@outlook.es>
2025-04-14 20:11:42 +0200

    initial commit

---
+++ src/main/java/com/dev/insurance_middle/InsuranceMiddleApplication.java
@@ -0,0 +1,13 @@
+package com.dev.insurance_middle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceMiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceMiddleApplication.class, args);
+	}
+
+}
